fdf0bc2e860c1ad641d6af08b9e26a65af0257dc
jefferson-jms <83091992+jefferson-jms@users.noreply.github.com>
2021-05-20 09:42:25 -0300

    criado footer e estilizado, adicionado alertas da página.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,6 @@ $(function(){ $("#cpf").mask("000.000.000-00");});
 $(function(){ $("#cep").mask("00.000-000");});
 $(function(){ $("#telefone").mask("(00) 0000-0000");});
 $(function(){ $("#celular").mask("(00) 00000-0000");});
+
+
+
