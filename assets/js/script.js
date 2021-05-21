c257b8f36c960c206bac7dd51d65f3e87c892069
jefferson-jms <83091992+jefferson-jms@users.noreply.github.com>
2021-05-20 21:32:35 -0300

    modificações finais em estilizações e adicionado mascara para o campo RG

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 $(function(){ $("#cpf").mask("000.000.000-00");});
+$(function(){ $("#rg").mask("0000000000");});
 $(function(){ $("#cep").mask("00.000-000");});
 $(function(){ $("#telefone").mask("(00) 0000-0000");});
 $(function(){ $("#celular").mask("(00) 00000-0000");});
